1f66b82362f8bdfeaddf095e048d7a3464de957f
Leo <leoyang388@gmail.com>
2023-12-29 20:04:32 -0800

    first commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://realpython.github.io/fake-jobs/"
+page = requests.get(URL)
+
+soup = BeautifulSoup(page.content, "html.parser")
+results = soup.find(id="ResultsContainer")
+
+search = input("What job would you like to be scrapped? ").lower()
+searchJobs = results.find_all(
+    "h2", string=lambda text: search in text.lower()
+)
+jobElements = [
+    h2_element.parent.parent.parent for h2_element in searchJobs
+]
+for job in jobElements:
+    title = job.find("h2", class_="title")
+    company = job.find("h3", class_="company")
+    location = job.find("p", class_="location")
+    link_url = job.find_all("a")[1]["href"]
+    print()
+    print(title.text.strip())
+    print(company.text.strip())
+    print(location.text.strip())
+    print(f"Apply here: {link_url}\n")
